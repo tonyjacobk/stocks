7e4b93f169d81b3aeb58de2846553067c1742267
tonyjacobk <tonyjacob@hotmail.com>
2025-07-07 17:49:44 +0530

    Create myapp.py
    Adding the file

---
+++ myapp.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+import pymysql
+from aiven import connect
+from math import ceil
+
+app = Flask(__name__)
+
+# MySQL configurations
+mysql=connect()
+@app.route('/')
+@app.route('/<int:page>')
+def index(page=1):
+    per_page = 20
+    cur = mysql.cursor()
+    cur.execute("SELECT COUNT(*) FROM reports")
+    total_rows = cur.fetchone()['COUNT(*)']
+    print(total_rows)
+    total_pages = ceil(total_rows / per_page)
+    offset = (page - 1) * per_page
+    print (offset)
+    cur.execute(f"SELECT * FROM reports LIMIT 20 OFFSET 0 ;")
+    data = cur.fetchall()
+    print (data)
+    cur.close()
+    return render_template('index.html', data=data, page=page, total_pages=total_pages)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',debug=True)
+
+
