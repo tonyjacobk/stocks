0f9c85fb79ce9956f03636f3439e05dbb4a8af59
Tony Jac <tonyjacob@hotmail.com>
2025-07-21 12:27:08 +0000

    New GUI

--- myapp.py
+++ myapp.py
@@ -1,29 +1,21 @@
 from flask import Flask, render_template
-import pymysql
-from aiven import connect
-from math import ceil
-
+from aiven import get_rows,get_broker
+import urllib.parse
 app = Flask(__name__)
-
+rows_per_page=20
 # MySQL configurations
-mysql=connect()
 @app.route('/')
 @app.route('/<int:page>')
 def index(page=1):
-    per_page = 20
-    cur = mysql.cursor()
-    cur.execute("SELECT COUNT(*) FROM reports")
-    total_rows = cur.fetchone()['COUNT(*)']
-    print(total_rows)
-    total_pages = ceil(total_rows / per_page)
-    offset = (page - 1) * per_page
-    print (offset)
-    cur.execute(f"SELECT * FROM reports ORDER BY report_date DESC  LIMIT 20 OFFSET %s", (offset) )
-    data = cur.fetchall()
-    print (data)
-    cur.close()
+    data,total_pages=get_rows(page,20)
     return render_template('index.html', data=data, page=page, total_pages=total_pages)
-
+@app.route('/brk/<brok>')
+@app.route('/brk/<brok>/<int:page>')
+def search_broker(brok,page=1):
+    brok=urllib.parse.unquote(brok)
+    print(brok)
+    data,total_pages=get_broker(page,rows_per_page,brok)
+    return render_template('index.html', data=data, page=page, total_pages=total_pages,broker=brok)
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
 
