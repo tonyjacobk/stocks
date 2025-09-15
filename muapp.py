8911ab5e23850dec875fb95dd39e8b73f0f21dd2
Tony Jac <tonyjacob@hotmail.com>
2025-09-15 05:34:05 +0000

    Testing

--- muapp.py
+++ muapp.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template,Blueprint
 from aiven import get_rows,get_broker,get_stock,get_stock_partial
 from price import get_price,load_price,get_date
+from download_bhav_copy  import bhav_main
+rows_per_page=20
+rows,res=load_price()
+report_date=get_date(rows)
 
 report_bap=Blueprint("report",__name__)
 
